03b679b38be0a2462459fc39e4605868f3e65a79
Manvel Avetisian <avetisian@gmail.com>
2018-03-22 20:21:40 +0300

    link VS Code files

--- make_links.sh
+++ make_links.sh
@@ -2,5 +2,10 @@
 
 for x in .zshrc .emacs .jupyter .gdbinit .tmux.conf
 do
-    ln -f -s ~/etc/$x ~/
+    ln -f -s ~/etc/$x ~/$x
+done
+
+for x in keybindings.json settings.json
+do
+    ln -f -s ~/etc/vscode/$x ~/.config/Code/User/$x
 done
