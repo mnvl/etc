b404085be1394e6a33860599d44743ce4fb390e6
Manvel Avetisian <avetisian@gmail.com>
2020-11-21 20:40:53 +0300

    clone tpm into different dir

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 #! /bin/sh -ex
 
+
 rm -rf $HOME/.oh-my-zsh
 
 sh -c "CHSH=no RUNZSH=no KEEP_ZSHRC=yes $(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -9,7 +10,15 @@ do
     git clone https://github.com/zsh-users/$plugin.git $HOME/.oh-my-zsh/custom/plugins/$plugin
 done
 
+
+rm -rf $HOME/.tmux/plugins/tpm
+
+mkdir -p ~/.tmux/plugins
+
+git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
+
+
 for x in .zshrc .emacs .jupyter .gdbinit .tmux.conf .gitconfig
 do
-    ln -f -s ~/etc/$x ~/$x
+    ln -f -s $HOME/etc/$x $HOME/$x
 done
