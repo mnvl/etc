e25e91917d75f4d2de96c87bea93574ded7e9c1f
Manvel Avetisian <manvel@airi.net>
2023-05-24 10:15:57 +0300

    install extra packages in init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,5 @@
 #! /bin/sh -ex
 
-
 rm -rf $HOME/.oh-my-zsh
 
 sh -c "CHSH=no RUNZSH=no KEEP_ZSHRC=yes $(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -22,3 +21,22 @@ for x in .zshrc .emacs .jupyter .gdbinit .tmux.conf .gitconfig
 do
     ln -f -s $HOME/etc/$x $HOME/$x
 done
+
+case "$(uname -s)" in
+    Linux*)
+        sudo apt-get install clangd
+    ;;
+
+    Darwin*)
+        brew install llvm
+    ;;
+
+    *)
+        echo "unknown OS, clangd was not installed"
+esac
+
+pip3 install 'python-lsp-server[all]'
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+cargo install rls
+rustup component add rls
