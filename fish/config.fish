480332b454d4a1aae67d88eb8310aa2cd3680c72
Manvel Avetisian <avetisian@gmail.com>
2025-10-22 12:57:12 +0300

    use iosevka in git gui

--- fish/config.fish
+++ fish/config.fish
@@ -3,3 +3,17 @@ if status is-interactive
    fish_add_path -g /opt/homebrew/bin/
    fish_add_path -g /usr/local/bin:$PATH
 end
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /home/sch/miniconda3/bin/conda
+    eval /home/sch/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+else
+    if test -f "/home/sch/miniconda3/etc/fish/conf.d/conda.fish"
+        . "/home/sch/miniconda3/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "/home/sch/miniconda3/bin" $PATH
+    end
+end
+# <<< conda initialize <<<
+
