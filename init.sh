6daef2d9106cee7e5e33c04e1a892a842546a193
Manvel Avetisian <avetisian@gmail.com>
2025-12-04 14:49:09 +0800

    install git-lfs as well

--- init.sh
+++ init.sh
@@ -5,7 +5,7 @@ case "$(uname -s)" in
         gsettings list-keys org.gnome.settings-daemon.plugins.media-keys | grep brightness \
             | xargs -I KEY gsettings set org.gnome.settings-daemon.plugins.media-keys KEY "[]"
 
-        sudo apt-get install fish mc emacs tmux clangd git-gui
+        sudo apt-get install fish mc emacs tmux clangd git-gui git-lfs
 
         curl -s 'https://api.github.com/repos/be5invis/Iosevka/releases/latest' | jq -r ".assets[] | .browser_download_url" | grep 'PkgTTC-Iosevka-.*zip' | xargs -n 1 curl -L -O --fail --silent --show-error
         unzip PkgTTC-Iosevka-*.zip
